a1b6ba70d62a194bc8da40cf5a5387eb148d8462
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-09-05 21:23:19 +0700

    :zap: update to recent work

--- Requests_Module/01-requests.py
+++ Requests_Module/01-requests.py
@@ -1,16 +1,23 @@
 import requests
+import json
 
 def main():
-    response = requests.get("https://api.exchangeratesapi.io/latest")
+    response = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu")
     
-    if response.status_code != 200:
+    if response.status_code == 200:
+        print("Status Code: ", response.status_code)
+        
+        # Get Headers
+        print(response.header)
+
+        json_res = json.loads(response.content)
+        # data = response.json()
+        print("Pokemon name is", json_res['name'])
+    
+    else:
         print("Status Code: ", response.status_code)
         raise Exception("There was an error!")
-
-    data = response.json()
-    print("JSON data: ", data)
-
-
+    
 
 if __name__ == "__main__":
     main()
