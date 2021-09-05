e4c84370454c840a70e3d6a2f4f82ac3447d2d93
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-09-05 17:23:25 +0700

    :fire: init

---
+++ Requests_Module/00-requests.py
@@ -0,0 +1,24 @@
+"""
+    A simple GET req using requests
+"""
+
+import requests
+from rich import style
+from rich.console import Console
+
+console = Console()
+
+def main():
+    response = requests.get("http://www.google.com")
+    # response = requests.get("http://www.google.com/random-address/")
+    console.print("Status Code: ", response.status_code, style="cyan2")
+    print('\n')
+    console.print("Headers: ", response.headers, style = "cyan2")
+    print('\n')
+    console.print("'Content-Type': ", response.headers['Content-Type'], style = "cyan2")
+    print('\n')
+    console.print("Content: ", response.text, style="cyan2")
+
+
+if __name__ == "__main__":
+    main()
